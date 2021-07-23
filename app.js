f78466d48a55f5eec60ac13fe702e999fc66bd3a
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-07-23 14:03:53 +0200

    get from local storage

--- app.js
+++ app.js
@@ -266,7 +266,7 @@ function savePalette() {
       updateTextUI(index);
     });
   });
-
+  // resetInputs();
   // Append to library
   palette.appendChild(title);
   palette.appendChild(preview);
@@ -295,5 +295,54 @@ function openLibrary() {
 function closeLibrary() {
   libraryContainer.classList.remove("active");
 }
+
+function getLocal() {
+  // let localPalettes;
+  if (localStorage.getItem("palettes") === null) {
+    localPallets = [];
+  } else {
+    const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+    paletteObjects.forEach((paletteObj) => {
+      //Generate the palette library
+      const palette = document.createElement("div");
+      palette.classList.add("custom-palette");
+      const title = document.createElement("h4");
+      title.innerText = paletteObj.name;
+      const preview = document.createElement("div");
+      preview.classList.add("small-preview");
+      paletteObj.colors.forEach((smallColor) => {
+        const smallDiv = document.createElement("div");
+        smallDiv.classList.add("preview-color");
+        preview.appendChild(smallDiv);
+        smallDiv.style.backgroundColor = smallColor;
+      });
+      const paletteBtn = document.createElement("button");
+      paletteBtn.classList.add("pick-palette-btn");
+      paletteBtn.classList.add(paletteObj.nr);
+      paletteBtn.innerText = "Select";
+
+      //Attach event to the btn
+
+      paletteBtn.addEventListener("click", (e) => {
+        closeLibrary();
+        const paletteIndex = e.target.classList[1];
+        initialColors = [];
+        paletteObjects[paletteIndex].colors.forEach((color, index) => {
+          initialColors.push(color);
+          colorDivs[index].style.backgroundColor = color;
+          const text = colorDivs[index].children[0];
+          updateTextUI(index);
+        });
+      });
+      // resetInputs();
+      // Append to library
+      palette.appendChild(title);
+      palette.appendChild(preview);
+      palette.appendChild(paletteBtn);
+      libraryContainer.children[0].appendChild(palette);
+    });
+  }
+}
+getLocal();
 
 randomColors();
